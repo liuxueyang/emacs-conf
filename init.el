1d758672ce964dda360d5f2ab20418d7d91b7c57
liuxueyang <zypz457@sina.com>
2017-12-10 18:41:23 +0800

    set vc follow symlink to true

--- init.el
+++ init.el
@@ -53,7 +53,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (lispy helm magit ace-jump-mode use-package)))
- '(use-package-always-ensure t))
+ '(use-package-always-ensure t)
+ '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
