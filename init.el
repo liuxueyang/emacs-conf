163ff3d3d5f2da86d2dca3bf79d916f6f80aa223
liuxueyang <zypz457@sina.com>
2017-12-11 01:26:32 +0800

    add support for lisp
    1. solarized-theme
    2. rainbow-delimiters
    3. rainbow-blocks
    4. hl-sexp
    5. diff-hl
    6. ace-window

--- init.el
+++ init.el
@@ -43,7 +43,10 @@
 	      `((top . 1) (left . 1) (width . ,frame-width) (height . ,width-height))
 	      fill-column 80)
 
-(load-theme 'tsdh-dark t)
+;; (load-theme 'tsdh-dark t)
+(use-package solarized-theme
+  :ensure t)
+(load-theme 'solarized-dark t)
 ;; (menu-bar-mode -1)
 (menu-bar-mode 1)
 (tool-bar-mode -1)
@@ -55,14 +58,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (lispy helm magit ace-jump-mode use-package)))
+ '(package-selected-packages
+   (quote
+    (diff-hl hl-sexp solarized-theme rainbow-blocks rainbow-delimiters lispy helm magit ace-jump-mode use-package)))
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rainbow-blocks-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-blocks-depth-2-face ((t (:foreground "orange"))))
+ '(rainbow-blocks-depth-3-face ((t (:foreground "blue"))))
+ '(rainbow-blocks-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-blocks-depth-5-face ((t (:foreground "cyan"))))
+ '(rainbow-blocks-depth-6-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-blocks-depth-7-face ((t (:foreground "violet"))))
+ '(rainbow-blocks-depth-8-face ((t (:foreground "green yellow"))))
+ '(rainbow-blocks-depth-9-face ((t (:foreground "DarkOrange2"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "violet"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "green yellow"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "DarkOrange2"))))
+ '(rainbow-delimiters-mismatched-face ((t (:inherit rainbow-delimiters-unmatched-face :background "green"))))
+ '(rainbow-delimiters-unmatched-face ((t (:background "yellow")))))
 
 (use-package ace-jump-mode
   :ensure t
@@ -90,3 +114,30 @@
   :ensure t
   :hook
   ((emacs-lisp-mode lisp-mode) . lispy-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook ((lisp-mode emacs-lisp-mode) . rainbow-delimiters-mode))
+
+(use-package rainbow-blocks
+  :ensure t
+  :hook ((lisp-mode emacs-lisp-mode) . rainbow-blocks-mode))
+
+(use-package hl-sexp
+  :ensure t
+  :hook ((lisp-mode emacs-lisp-mode) . hl-sexp-mode))
+
+(use-package diff-hl
+  :ensure t
+  :hook ((prog-mode . turn-on-diff-hl-mode)
+	 (dired-mode . diff-hl-dired-mode))
+  :init
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
+
+(use-package ace-window
+  :ensure t
+  :bind
+  (("M-p" . ace-window))
+  :custom
+  (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
