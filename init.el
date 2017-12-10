699d9af99383758e9398b7e66d92ed4ad4f4ab05
liuxueyang <zypz457@sina.com>
2017-12-10 22:45:47 +0800

    add rust support

--- init.el
+++ init.el
@@ -55,7 +55,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (lispy helm magit ace-jump-mode use-package)))
+ '(package-selected-packages
+   (quote
+    (rust-playground rust lispy helm magit ace-jump-mode use-package)))
  '(use-package-always-ensure t)
  '(vc-follow-symlinks t))
 (custom-set-faces
@@ -87,3 +89,8 @@
 (use-package lispy
   :hook
   ((emacs-lisp-mode lisp-mode) . lispy-mode))
+
+(use-package rust-mode
+  :mode "\\.rs\\'")
+
+(use-package rust-playground)
