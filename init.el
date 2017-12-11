94d2d2ec9d9c277c79a6bc66591163dfaf38cea5
liuxueyang <zypz457@sina.com>
2017-12-11 15:31:14 +0800

    change delimiters blue to SkyBlue1

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
  '(rainbow-blocks-depth-9-face ((t (:foreground "DarkOrange2"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "SkyBlue1"))))
  '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
  '(rainbow-delimiters-depth-5-face ((t (:foreground "cyan"))))
  '(rainbow-delimiters-depth-6-face ((t (:foreground "deep sky blue"))))
