224b2c98b8ea1362ef371be3597c74084238ec05
liuxueyang <zypz457@sina.com>
2017-12-27 20:34:01 +0800

    add document link for markdown mode

--- init.el
+++ init.el
@@ -289,6 +289,7 @@
 ;; --------------------------------------------------
 ;; Markdown
 (use-package markdown-mode
+  "https://jblevins.org/projects/markdown-mode/"
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
