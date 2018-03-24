8196aff830daa0e5c76520e5977d374035d7af92
liuxueyang <zypz457@sina.com>
2018-03-24 23:07:10 +0800

    disable source ~/.zshrc

--- init.el
+++ init.el
@@ -43,24 +43,24 @@
 	      `((top . 1) (left . 1) (width . ,frame-width) (height . ,width-height))
 	      fill-column 80)
 
-(when (or (string-equal system-type "darwin")
-	  (string-equal system-type "gnu/linux"))
-  (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
-    (setenv "PATH" path)
-    (setq exec-path
-	  (append
-	   (split-string-and-unquote path ":")
-	   exec-path))
-    ;; used by pyvenv plugin
-    (setenv "WORKON_HOME"
-	    (shell-command-to-string ". ~/.zshrc; echo -n $WORKON_HOME"))
-    (setenv
-     "VIRTUALENVWRAPPER_PYTHON"
-     (shell-command-to-string ". ~/.zshrc; echo -n $VIRTUALENVWRAPPER_PYTHON"))
-    ;; used by elpy
-    (setenv
-     "PATH_TO_PYTHON"
-     (shell-command-to-string ". ~/.zshrc; echo -n $PATH_TO_PYTHON"))))
+;; (when (or (string-equal system-type "darwin")
+;; 	  (string-equal system-type "gnu/linux"))
+;;   (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+;;     (setenv "PATH" path)
+;;     (setq exec-path
+;; 	  (append
+;; 	   (split-string-and-unquote path ":")
+;; 	   exec-path))
+;;     ;; used by pyvenv plugin
+;;     (setenv "WORKON_HOME"
+;; 	    (shell-command-to-string ". ~/.zshrc; echo -n $WORKON_HOME"))
+;;     (setenv
+;;      "VIRTUALENVWRAPPER_PYTHON"
+;;      (shell-command-to-string ". ~/.zshrc; echo -n $VIRTUALENVWRAPPER_PYTHON"))
+;;     ;; used by elpy
+;;     (setenv
+;;      "PATH_TO_PYTHON"
+;;      (shell-command-to-string ". ~/.zshrc; echo -n $PATH_TO_PYTHON"))))
 
 ;; (load-theme 'tsdh-dark t)
 (use-package solarized-theme
