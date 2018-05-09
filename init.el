21daecb02f1469c92c551545a72f7e323549970a
liuxueyang <zypz457@sina.com>
2018-05-09 22:36:29 +0800

    configure plugins for c++/c

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
 ;; taken care of by ‘package-initialize’.
 
 (package-initialize)
-;; TODO: install use-package automatically if necessary
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; different configuration for different platform
@@ -43,27 +42,6 @@
 	      `((top . 1) (left . 1) (width . ,frame-width) (height . ,width-height))
 	      fill-column 80)
 
-;; (when (or (string-equal system-type "darwin")
-;; 	  (string-equal system-type "gnu/linux"))
-;;   (let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
-;;     (setenv "PATH" path)
-;;     (setq exec-path
-;; 	  (append
-;; 	   (split-string-and-unquote path ":")
-;; 	   exec-path))
-;;     ;; used by pyvenv plugin
-;;     (setenv "WORKON_HOME"
-;; 	    (shell-command-to-string ". ~/.zshrc; echo -n $WORKON_HOME"))
-;;     (setenv
-;;      "VIRTUALENVWRAPPER_PYTHON"
-;;      (shell-command-to-string ". ~/.zshrc; echo -n $VIRTUALENVWRAPPER_PYTHON"))
-;;     ;; used by elpy
-;;     (setenv
-;;      "PATH_TO_PYTHON"
-;;      (shell-command-to-string ". ~/.zshrc; echo -n $PATH_TO_PYTHON"))))
-
-(use-package solarized-theme
-  :ensure t)
 (use-package dracula-theme
   :ensure t)
 
@@ -71,7 +49,7 @@
 ;; (load-theme 'solarized-dark t)
 (load-theme 'dracula t)
 
-(menu-bar-mode -1)
+;; (menu-bar-mode -1)
 ;; (menu-bar-mode 1)
 (tool-bar-mode -1)
 (column-number-mode 1)
@@ -81,8 +59,10 @@
 (display-time-mode 1)
 (setq-default display-time-day-and-date t)
 
-(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
-(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+;; transparent frame setting
+
+;; (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+;; (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 
 (add-hook 'org-mode-hook 'auto-fill-mode)
 
@@ -171,7 +151,7 @@
 (use-package counsel
   :ensure t
   :bind
-  (("M-x" . counsel-M-x)
+  (;; ("M-x" . counsel-M-x)
    ("C-x C-f" . counsel-find-file)
    ("C-c k" . counsel-ag)))
 
@@ -213,35 +193,6 @@
   (aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
 ;; --------------------------------------------------
-;; rust
-(use-package rust-mode
-  :ensure t
-  :mode "\\.rs\\'")
-
-(use-package rust-playground
-  :ensure t)
-;; --------------------------------------------------
-
-;; --------------------------------------------------
-;; python
-(use-package elpy
-  :ensure t
-  :config
-  (elpy-enable)
-  :custom
-  (;; (elpy-rpc-python-command (getenv ""))
-   (elpy-rpc-backend "rope")))
-
-(use-package py-yapf
-  :ensure t
-  :hook
-  (python-mode . py-yapf-enable-on-save))
-
-(use-package sphinx-doc
-  :ensure t
-  :hook
-  (python-mode . sphinx-doc-mode))
-;; --------------------------------------------------
 
 (use-package yasnippet-snippets
   :ensure t)
@@ -252,20 +203,6 @@
   (setq yas-snippet-dirs
 	'("~/.emacs.d/snippets"))
   (yas-global-mode 1))
-
-(use-package auctex
-  :ensure t
-  :no-require t
-  :custom
-  ((TeX-auto-save t)
-   (TeX-parse-self t)
-   (TeX-master nil)
-   (reftex-plug-into-AUCTeX t))
-  :hook
-  ((LaTeX-mode . auto-fill-mode)
-   (LaTeX-mode . flyspell-mode)
-   (LaTeX-mode . LaTeX-math-mode)
-   (LaTeX-mode . turn-on-reftex)))
 
 (use-package multiple-cursors
   :ensure t
@@ -283,23 +220,6 @@
   :bind
   (("C-s" . swiper-helm)))
 
-(use-package yaml-mode
-  :ensure t
-  :mode "\\.yaml\\'"
-  :bind
-  ("C-m" . newline-and-indent))
-
-;; --------------------------------------------------
-;; OCaml
-;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
-
-(let ((opam-user-setup-path "~/.emacs.d/opam-user-setup.el"))
-  (when (file-exists-p opam-user-setup-path)
-    (require 'opam-user-setup opam-user-setup-path)))
-
-;; ## end of OPAM user-setup addition for emacs / base ## keep this line
-;; --------------------------------------------------
-
 ;; --------------------------------------------------
 ;; Markdown
 ;; "https://jblevins.org/projects/markdown-mode/"
@@ -315,4 +235,215 @@
 ;; --------------------------------------------------
 ;; C
 (add-hook 'c-mode-hook (lambda () (c-set-style "linux")))
+;; --------------------------------------------------
+
+;; --------------------------------------------------
+;; gtags
+
+;; (use-package ggtags
+;;   :ensure t)
+
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;;               (ggtags-mode 1))))
+
+;; (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;; (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+;; (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+;; (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+;; (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+;; (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+;; (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;; --------------------------------------------------
+
+(use-package function-args
+  :ensure t
+  :config
+  (fa-config-default))
+
+(use-package helm
+  :ensure t
+  :init
+  (progn
+    (require 'helm-config)
+    (require 'helm-grep)
+    ;; To fix error at compile:
+    ;; Error (bytecomp): Forgot to expand macro with-helm-buffer in
+    ;; (with-helm-buffer helm-echo-input-in-header-line)
+    (if (version< "26.0.50" emacs-version)
+        (eval-when-compile (require 'helm-lib)))
+
+    (defun helm-hide-minibuffer-maybe ()
+      (when (with-helm-buffer helm-echo-input-in-header-line)
+        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+          (overlay-put ov 'window (selected-window))
+          (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                                  `(:background ,bg-color :foreground ,bg-color)))
+          (setq-local cursor-type nil))))
+
+    (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+    ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+    ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+    ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+    (global-set-key (kbd "C-c h") 'helm-command-prefix)
+    (global-unset-key (kbd "C-x c"))
+
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+    (define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+    (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+    (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
+
+    (setq helm-google-suggest-use-curl-p t
+          helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+          ;; helm-quick-update t ; do not display invisible candidates
+          helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+
+          ;; you can customize helm-do-grep to execute ack-grep
+          ;; helm-grep-default-command "ack-grep -Hn --smart-case --no-group --no-color %e %p %f"
+          ;; helm-grep-default-recurse-command "ack-grep -H --smart-case --no-group --no-color %e %p %f"
+          helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+
+          helm-echo-input-in-header-line nil
+
+          ;; helm-candidate-number-limit 500 ; limit the number of displayed canidates
+          helm-ff-file-name-history-use-recentf t
+          helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+          helm-buffer-skip-remote-checking t
+
+          helm-mode-fuzzy-match t
+
+          helm-buffers-fuzzy-matching t ; fuzzy matching buffer names when non-nil
+                                        ; useful in helm-mini that lists buffers
+          helm-org-headings-fontify t
+          ;; helm-find-files-sort-directories t
+          ;; ido-use-virtual-buffers t
+          helm-semantic-fuzzy-match t
+          helm-M-x-fuzzy-match t
+          helm-imenu-fuzzy-match t
+          helm-lisp-fuzzy-completion t
+          ;; helm-apropos-fuzzy-match t
+          helm-buffer-skip-remote-checking t
+          helm-locate-fuzzy-match t
+          helm-display-header-line nil)
+
+    (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+    (global-set-key (kbd "C-x b") 'helm-buffers-list)
+    ;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (global-set-key (kbd "C-c r") 'helm-recentf)
+    (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+    (global-set-key (kbd "C-c h o") 'helm-occur)
+    (global-set-key (kbd "C-c h o") 'helm-occur)
+
+    (global-set-key (kbd "C-c h w") 'helm-wikipedia-suggest)
+    (global-set-key (kbd "C-c h g") 'helm-google-suggest)
+
+    (global-set-key (kbd "C-c h x") 'helm-register)
+    ;; (global-set-key (kbd "C-x r j") 'jump-to-register)
+
+    (define-key 'help-command (kbd "C-f") 'helm-apropos)
+    (define-key 'help-command (kbd "r") 'helm-info-emacs)
+    (define-key 'help-command (kbd "C-l") 'helm-locate-library)
+
+    ;; use helm to list eshell history
+    (add-hook 'eshell-mode-hook
+              #'(lambda ()
+                  (define-key eshell-mode-map (kbd "M-l")  'helm-eshell-history)))
+
+;;; Save current position to mark ring
+    (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+
+    ;; show minibuffer history with Helm
+    (define-key minibuffer-local-map (kbd "M-p") 'helm-minibuffer-history)
+    (define-key minibuffer-local-map (kbd "M-n") 'helm-minibuffer-history)
+
+    (define-key global-map [remap find-tag] 'helm-etags-select)
+
+    (define-key global-map [remap list-buffers] 'helm-buffers-list)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; PACKAGE: helm-swoop                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; Locate the helm-swoop folder to your path
+    (use-package helm-swoop
+      :bind (("C-c h o" . helm-swoop)
+             ("C-c s" . helm-multi-swoop-all))
+      :config
+      ;; When doing isearch, hand the word over to helm-swoop
+      (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+
+      ;; From helm-swoop to helm-multi-swoop-all
+      (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+
+      ;; Save buffer when helm-multi-swoop-edit complete
+      (setq helm-multi-swoop-edit-save t)
+
+      ;; If this value is t, split window inside the current window
+      (setq helm-swoop-split-with-multiple-windows t)
+
+      ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+      (setq helm-swoop-split-direction 'split-window-vertically)
+
+      ;; If nil, you can slightly boost invoke speed in exchange for text color
+      (setq helm-swoop-speed-or-color t))
+
+    (helm-mode 1)
+
+    (use-package helm-projectile
+      :ensure t
+      :init
+      (helm-projectile-on)
+      (setq projectile-completion-system 'helm)
+      (setq projectile-indexing-method 'alien))))
+
+;; --------------------------------------------------
+;; helm-gtags
+
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t)
+
+(use-package helm-gtags
+  :ensure t)
+
+;; (require 'helm-gtags)
+;; Enable helm-gtags-mode
+
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+
+;; --------------------------------------------------
+
+;; --------------------------------------------------
+;; sr-speedbar
+
+(use-package sr-speedbar
+  :ensure t)
+(global-set-key (kbd "C-x p") 'sr-speedbar-toggle)
+
 ;; --------------------------------------------------
